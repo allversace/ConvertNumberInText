fa4d3014e863c574d2ef81ceeaf8f8523e8182d7
Артур <119939945+allversace@users.noreply.github.com>
2023-04-13 14:15:42 +0300

    Update ConvertNumberInText.xaml.cs

--- ConvertNumberInText.xaml.cs
+++ ConvertNumberInText.xaml.cs
@@ -17,87 +17,52 @@ namespace WpfApp13
 {
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            writeText.MaxLength = 3;
-        }
+        private string[] oneCount = new string[9] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        private string[] twoCount = new string[10] { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        private string[] threeCount = new string[8] { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
 
-        private void Close_Click(object sender, RoutedEventArgs e)
+    public MainWindow()
+    {
+        InitializeComponent();
+        writeText.MaxLength = 3;
+    }
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void Search_Click(object sender, RoutedEventArgs e)
+    {
+        int number;
+        if (int.TryParse(writeText.Text, out number) && number > 0 && number <= 100)
         {
-            Close();
-        }
-        
-        private void Search_Click(object sender, RoutedEventArgs e)
-        {
-            if (int.Parse(writeText.Text) <= 100 && int.Parse(writeText.Text) > 0)
+            if (number < 10)
             {
-                int a = int.Parse(writeText.Text);
-                string[] oneCount = new string[9] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-                string[] twoCount = new string[10] { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-                string[] threeCount = new string[8] { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-                if (a < 10)
+                result.Text = oneCount[number - 1];
+            }
+            else if (number < 20)
+            {
+                result.Text = twoCount[number - 10];
+            }
+            else
+            {
+                int tens = number / 10;
+                int ones = number % 10;
+
+                if (ones == 0)
                 {
-                    result.Text = oneCount[a - 1];
+                    result.Text = threeCount[tens - 2];
                 }
-                else if (a == 10) 
-                    result.Text = twoCount[0];
-                else if (a < 20)
+                else
                 {
-                    result.Text = twoCount[a - 10];
+                    result.Text = threeCount[tens - 2] + " " + oneCount[ones - 1];
                 }
-                else if (a == 20) 
-                    result.Text = threeCount[0];
-                else if (a < 30)
-                {
-                    result.Text = threeCount[0] + " " + oneCount[a - 21];
-                }
-                else if (a == 30) 
-                    result.Text = threeCount[1];
-                else if (a < 40)
-                {
-                    result.Text = threeCount[1] + " " + oneCount[a - 31];
-                }
-                else if (a == 40) 
-                    result.Text = threeCount[2];
-                else if (a < 50)
-                {
-                    result.Text = threeCount[2] + " " + oneCount[a - 41];
-                }
-                else if (a == 50) 
-                    result.Text = threeCount[3];
-                else if (a < 60)
-                {
-                    result.Text = threeCount[3] + " " + oneCount[a - 51];
-                }
-                else if (a == 60) 
-                    result.Text = threeCount[4];
-                else if (a < 70)
-                {
-                    result.Text = threeCount[4] + " " + oneCount[a - 61];
-                }
-                else if (a == 70) 
-                    result.Text = threeCount[5];
-                else if (a < 80)
-                {
-                    result.Text = threeCount[5] + " " + oneCount[a - 71];
-                }
-                else if (a == 80) 
-                    result.Text = threeCount[6];
-                else if (a < 90)
-                {
-                    result.Text = threeCount[6] + " " + oneCount[a - 81];
-                }
-                else if (a == 90) 
-                    result.Text = threeCount[7];
-                else if (a < 100)
-                {
-                    result.Text = threeCount[7] + " " + oneCount[a - 91];
-                }
-                else if (a == 100)
-                {
-                    result.Text = "one hundred";
-                }
+            }
+
+            if (number == 100)
+            {
+                result.Text = "one hundred";
             }
         }
     }
